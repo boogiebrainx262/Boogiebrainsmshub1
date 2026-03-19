5e28bc3b52d391f81103b631916a3e1402407b5f
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-03-19 10:28:10 +0100

    Create Rules.js

---
+++ frontend/src/pages/Rules.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export default function Rules() {
+  return (
+    <div>
+      <h2>Disclaimer</h2>
+      <p>- The accounts we sell are original and exclusively provided to you.</p>
+      <p>- Once sold, accounts will never be resold or reused.</p>
+      <p>- Responsibility for account usage lies entirely with the buyer.</p>
+      <p>- We are not responsible for suspensions or restrictions caused by improper use.</p>
+      <p>- For secure usage, log in from trusted devices and clean IP addresses.</p>
+    </div>
+  );
+}
