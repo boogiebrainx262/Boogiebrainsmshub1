7daf4aac192e60821d1df167bcc1cd69728864ac
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-03-19 09:35:07 +0100

    Create Product.js

---
+++ backend/models/Product.js
@@ -0,0 +1,12 @@
+const mongoose = require("mongoose");
+
+const ProductSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  price: Number,
+  quantity: Number,
+  category: String,
+  image: String
+});
+
+module.exports = mongoose.model("Product", ProductSchema);
