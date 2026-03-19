ae62b0c3fa2a3703d1b53486b680b235e08aec9e
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-03-19 10:17:01 +0100

    Create Products.js

---
+++ frontend/src/pages/Products.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+export default function Products() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    axios.get("http://localhost:5000/api/products")
+      .then(res => setProducts(res.data))
+      .catch(err => console.error(err));
+  }, []);
+
+  return (
+    <div>
+      <h2>Products</h2>
+      {products.map(p => (
+        <div key={p._id}>
+          <h3>{p.name}</h3>
+          <p>{p.description}</p>
+          <p>₦{p.price}</p>
+          <button>Buy</button>
+        </div>
+      ))}
+    </div>
+  );
+                    }
