41b1b3bbef266bd4b8b20c0c7bd2a83eed58890d
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-03-19 10:13:24 +0100

    Create Dashboard.js

---
+++ frontend/src/pages/Dashboard.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+export default function Dashboard() {
+  return (
+    <div>
+      <h2>Dashboard</h2>
+      <p>Total Orders: 0</p>
+      <p>Total Deposits: ₦0.00</p>
+      <p>Wallet Balance: ₦0.00</p>
+    </div>
+  );
+}
