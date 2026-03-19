d2a2c81c8b82e79f47d4c183ef2bcbb78810e5db
boogiebrainx262 <adelajaayomidealiu@gmail.com>
2026-03-19 10:27:15 +0100

    Create Terms.js

---
+++ frontend/src/pages/Terms.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Terms() {
+  return (
+    <div>
+      <h2>Terms of Use</h2>
+      <p>Make sure to use m.facebook.com for first login...</p>
+      <p>Do not use Safari browser for first login...</p>
+      <p>Most recommended VPN: Surfshark and HMA VPN...</p>
+      <p>We cannot replace accounts for certain errors...</p>
+    </div>
+  );
+}
